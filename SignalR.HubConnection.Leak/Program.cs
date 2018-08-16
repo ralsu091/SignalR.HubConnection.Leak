3bff63d9d8cd85ca2abc0b68ecb022f98a4fed96
Rayan Alsubhi <rayan.alsubhi@feenics.com>
2018-08-16 14:16:04 -0400

    Add project files.

---
+++ SignalR.HubConnection.Leak/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using Nito.AsyncEx;
+
+namespace SignalR.HubConnection.Leak
+{
+    class Program
+    {
+        private static Subscriber Subscriber { get; set; }
+
+        static void Main(string[] args)
+        {
+            AsyncContext.Run(() => MainAsync(args));
+          
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            await StartChangesListener();
+        }
+
+        private static async Task StartChangesListener()
+        {
+           
+            try
+            {
+                await InitiateSubscriber();
+            }
+            catch (Exception e)
+            {
+            }
+            Task.Run(()=>MonitorSubscriber());
+        }
+
+        private static async Task InitiateSubscriber()
+        {
+            try
+            {
+                if (Subscriber != null)
+                {
+                    try
+                    {
+                        Subscriber.DisconnectFromSignal(TimeSpan.FromSeconds(5));
+                    }
+                    catch (NullReferenceException e)
+                    {
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+                Subscriber = new Subscriber(AddToQueue);
+                await Subscriber.ConnectForSignalAsync("EventsHub");
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void AddToQueue(EventDataEventArgs e)
+        {
+           
+        }
+
+        private static async Task MonitorSubscriber()
+        {
+         
+            while (true)
+            {
+               
+                try
+                {
+                    var isDisconnected = Subscriber.HubConnection == null ||
+                                         Subscriber.HubConnection.State == ConnectionState.Disconnected;
+                    if (isDisconnected)
+                    {
+                     
+                      
+                        await InitiateSubscriber();
+                    }
+                    
+                    else if (Subscriber.HubConnection.State == ConnectionState.Connected)
+                    {
+                        try
+                        {
+                            var whoAmI = await Subscriber.WhoAmI();
+                            if (!whoAmI.StartsWith("For Connection", StringComparison.InvariantCultureIgnoreCase))
+                            {
+                               
+                         
+                                await InitiateSubscriber();
+                            }
+                            
+
+                        }
+                        catch (Exception e)
+                        {
+                            await InitiateSubscriber();
+                        }
+                    }
+                    await Task.Delay(TimeSpan.FromSeconds(15));
+                }
+                catch (Exception exception)
+                {
+                  
+                }
+            }
+        }
+    }
+}
---
+++ SignalR.HubConnection.Leak/Subscriber.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SignalR.HubConnection.Leak
+{
+    public class Subscriber
+    {
+        public Subscriber(Action<EventDataEventArgs> onEventMessageAction)
+        {
+            if (onEventMessageAction != null)
+            {
+                OnEventMessageAction = onEventMessageAction;
+            }
+        }
+        public Microsoft.AspNet.SignalR.Client.HubConnection HubConnection { get; set; }
+        public IHubProxy HubProxy { get; set; }
+        private Action<EventDataEventArgs> OnEventMessageAction { get; set; }
+
+        public async Task<bool> ConnectForSignalAsync(string hubName, TextWriter traceOutput = null)
+        {
+            
+            HubConnection = new Microsoft.AspNet.SignalR.Client.HubConnection("http://serverdown.bogus.address.com", new Dictionary<string, string> { { "bearerToken", "Random" } });
+            
+            if (traceOutput != null)
+            {
+                HubConnection.TraceLevel = TraceLevels.All;
+
+                HubConnection.TraceWriter = traceOutput;
+            }
+            
+            HubProxy = HubConnection.CreateHubProxy(hubName);
+
+           HubProxy.On<object>("OnEventMessage",  OnEventMessage);
+            try
+            {
+                await HubConnection.Start();
+                return true;
+            }
+            catch (HttpRequestException e)
+            {
+                return false;
+            }
+        }
+        
+        public void DisconnectFromSignal(TimeSpan? timeout = null)
+        {
+            if (HubConnection == null)
+                throw new NullReferenceException("HubConnection is null.  Did you forget to call ConnectForSignalAsync");
+            if (timeout.HasValue)
+            {
+                HubConnection.Stop(timeout.Value);
+                HubConnection.Dispose();
+               
+            }
+            else
+            {
+                HubConnection.Stop();
+                HubConnection.Dispose();
+            }
+            OnEventMessageAction = null;
+         
+            HubConnection = null;
+            HubProxy = null;
+        }
+
+
+        public async Task<string> WhoAmI()
+        {
+            var result = await HubProxy.Invoke<string>("WhoAmI");
+            return result;
+        }
+        public async Task<string> WhoAreYou()
+        {
+            var result = await HubProxy.Invoke<string>("WhoAreYou");
+            return result;
+        }
+
+        private void OnEventMessage(object data)
+        {
+            OnEventMessageAction?.Invoke(new EventDataEventArgs {Data = data});
+
+        }
+    }
+    public class EventDataEventArgs : EventArgs
+    {
+        public object Data { get; set; }
+    }
+}
